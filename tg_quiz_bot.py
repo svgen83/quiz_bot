48025697beff68faf59c7c5d7d883e717b3a1183
svg83 <svgeneraloff@gmail.com>
2022-09-09 13:16:19 +0400

    удалена общая функция, отформатирован код

--- tg_quiz_bot.py
+++ tg_quiz_bot.py
@@ -6,81 +6,75 @@ import redis
 
 from dotenv import load_dotenv
 from quiz_base_tools import get_text_fragments, get_quiz_bases
+from quiz_base_tools import get_user_info
 
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Updater, CallbackContext, ConversationHandler
 from telegram.ext import CommandHandler, MessageHandler, Filters
 
-
 logger = logging.getLogger(__name__)
 
 CHOOSING, TYPING_REPLY = range(2)
 
-   
-def get_user_info (update, context):
-    chat_id = update.message.chat_id
-    if r.get(chat_id):
-        user_info = json.loads(r.get(chat_id))
-    return user_info
 
-
-def start(update, context):  
-    custom_keyboard = [['Новый вопрос', 'Сдаться'],['Мой счёт']]
+def start(update, context):
+    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_text('Привет! Я бот для викторин',
                               reply_markup=reply_markup)
     return CHOOSING
-    
+
 
 def handle_new_question_request(update, context):
-    user_info = get_user_info(update, context)
+    user_info = get_user_info(update.message.chat.id, redis_call)
+    quiz_bases = get_quiz_bases('quiz-questions')
     random_question, answer = random.choice(list(quiz_bases.items()))
-##    answer = quiz_bases.get(random_question)
-    short_answer = answer[answer.find(':')+2 : answer.find('.')]
-    user_info.update({'chat_id':update.message.chat_id,
-                 'question':random_question,
-                 'answer':short_answer
-                  })
-    r.set(user_info['chat_id'], json.dumps(user_info))
-    logger.info (user_info['question'])
-    logger.info (user_info['answer'])
+    short_answer = answer[answer.find(':') + 2:answer.find('.')]
+    user_info.update({
+        'chat_id': update.message.chat.id,
+        'question': random_question,
+        'answer': short_answer
+    })
+    redis_call.set(user_info['chat_id'], json.dumps(user_info))
+    logger.info(user_info['question'])
+    logger.info(user_info['answer'])
     update.message.reply_text(user_info['question'])
     return TYPING_REPLY
-    
-    
+
+
 def handle_solution_attempt(update, context):
-    user_info = get_user_info(update, context)
+    user_info = get_user_info(update.message.chat.id, redis_call)
     if update.message.text == user_info['answer']:
-        update.message.reply_text(''' Правильно! Поздравляю! 
+        update.message.reply_text(''' Правильно! Поздравляю!
                                       Для следующего вопроса нажми 'Новый вопрос' 
                                   ''')
         if 'score' in user_info:
             score = user_info['score']
         else:
             score = 0
-        score +=1
+        score += 1
         user_info['score'] = score
-        r.set(user_info['chat_id'], json.dumps(user_info))
+        redis_call.set(user_info['chat_id'], json.dumps(user_info))
         return CHOOSING
     else:
         update.message.reply_text('Неправильно... Попробуешь ещё раз?')
-        return TYPING_REPLY 
-       
+        return TYPING_REPLY
+
 
 def handle_hands_up(update, context):
-    user_info = get_user_info(update, context)
+    user_info = get_user_info(update.message.chat.id, redis_call)
     update.message.reply_text(user_info['answer'])
-    return CHOOSING  
-         
+    return CHOOSING
+
 
 def send_score(update, context):
-    user_info = get_user_info(update, context)
+    user_info = get_user_info(update.message.chat.id, redis_call)
     if user_info['score']:
         score = user_info['score']
     else:
         score = 0
     update.message.reply_text(f'Ваш счет:{score}')
-    return CHOOSING 
+    return CHOOSING
 
 
 def error(update, context):
@@ -88,48 +82,53 @@ def error(update, context):
 
 
 def start_bot():
-    
+    load_dotenv()
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+    tg_token = os.getenv('TG_TOKEN')
+
+    global redis_call
+
+    redis_call = redis.Redis(host=os.getenv('REDIS_ENDPOINT'),
+                             port=os.getenv('REDIS_PORT'),
+                             password=os.getenv('REDIS_PASSWORD'),
+                             db=0)
+
     updater = Updater(tg_token)
-     
+
     dp = updater.dispatcher
-    
+
     conv_handler = ConversationHandler(
         entry_points=[
-                      CommandHandler('start', start),
-                      MessageHandler(Filters.regex(r'Новый вопрос'),
-                                     handle_new_question_request),
-                      MessageHandler(Filters.regex(r'Мой счёт'),
-                                     send_score),
-                      MessageHandler(Filters.regex(r'Сдаться'),
-                                     handle_hands_up)
-                      ],
-
+            CommandHandler('start', start),
+            MessageHandler(Filters.regex(r'Новый вопрос'),
+                           handle_new_question_request),
+            MessageHandler(Filters.regex(r'Мой счёт'), send_score),
+            MessageHandler(Filters.regex(r'Сдаться'), handle_hands_up)
+        ],
         states={
             CHOOSING: [
-                       MessageHandler(Filters.regex(r'Новый вопрос'),
-                                      handle_new_question_request),
-                       MessageHandler(Filters.regex(r'Сдаться'),
-                                      handle_hands_up),
-                       MessageHandler(Filters.regex(r'Мой счёт'),
-                                      send_score)
-                       ],
-
+                MessageHandler(Filters.regex(r'Новый вопрос'),
+                               handle_new_question_request),
+                MessageHandler(Filters.regex(r'Сдаться'), handle_hands_up),
+                MessageHandler(Filters.regex(r'Мой счёт'), send_score)
+            ],
             TYPING_REPLY: [
-                           MessageHandler(Filters.text 
-                                          & ~Filters.regex(r'Мой счёт')
-                                          & ~Filters.regex(r'Сдаться'),
-                                          handle_solution_attempt),
-                           MessageHandler(Filters.regex(r'Мой счёт'),
-                                          send_score),            
-                           MessageHandler(Filters.regex(r'Сдаться'),
-                                          handle_hands_up)
-                           ]
+                MessageHandler(
+                    Filters.text & ~
+                    Filters.regex(r'Мой счёт') & ~
+                    Filters.regex(r'Сдаться'), handle_solution_attempt),
+                MessageHandler(Filters.regex(r'Мой счёт'), send_score),
+                MessageHandler(Filters.regex(r'Сдаться'), handle_hands_up)
+            ]
         },
-
-        fallbacks=[MessageHandler(Filters.regex(r'Мой счёт'), send_score),            
-                   MessageHandler(Filters.regex(r'Сдаться'), handle_hands_up)]
-        
-    )
+        fallbacks=[
+            MessageHandler(Filters.regex(r'Мой счёт'), send_score),
+            MessageHandler(Filters.regex(r'Сдаться'), handle_hands_up)
+        ])
 
     dp.add_handler(conv_handler)
     dp.add_error_handler(error)
@@ -138,18 +137,4 @@ def start_bot():
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    
-    logging.basicConfig(
-                  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                  level=logging.INFO
-                       )
-    
-    tg_token = os.getenv('TG_TOKEN')
-    
-    r = redis.Redis(host=os.getenv('REDIS_ENDPOINT'),
-                    port=os.getenv('REDIS_PORT'),
-                    password=os.getenv('REDIS_PASSWORD'), db=0)
-                    
-    quiz_bases = get_quiz_bases('quiz-questions')
     start_bot()
--- vk_quiz_bot.py
+++ vk_quiz_bot.py
@@ -8,143 +8,110 @@ import vk_api as vk
 
 from dotenv import load_dotenv
 from quiz_base_tools import get_text_fragments, get_quiz_bases
+from quiz_base_tools import get_user_info
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
-
 logger = logging.getLogger(__name__)
 
-        
-def get_user_info (event, vk_api):
-    user_info = {"user_id": event.user_id}
-    if r.get(event.user_id):
-        user_info = json.loads(r.get(event.user_id))
-    return user_info
 
-
-##def choose_msg(event, vk_api):
-##    if event.text == 'Новый вопрос':
-##        msg = handle_new_question_request(event, vk_api)
-##    elif event.text == 'Сдаться':
-##        msg = handle_hands_up(event, vk_api)
-##    elif event.text == 'Мой счёт':
-##        msg = send_score(event, vk_api)
-##    else: msg = handle_solution_attempt(event, vk_api)
-##    return msg
-
-
-def handle_new_question_request(event, vk_api):
-    user_info = get_user_info(event, vk_api)
+def handle_new_question_request(event, vk_api, redis_call):
+    quiz_bases = get_quiz_bases('quiz-questions')
+    user_info = get_user_info(event.user_id, redis_call)
     random_question = random.choice(list(quiz_bases))
     answer = quiz_bases.get(random_question)
-    short_answer = answer[answer.find(':')+2 : answer.find('.')]
-    user_info.update({'chat_id': event.user_id,
-                 'question':random_question,
-                 'answer':short_answer
-                  })
-    r.set(user_info['chat_id'], json.dumps(user_info))
-    logger.info (user_info['question'])
-    logger.info (user_info['answer'])
+    short_answer = answer[answer.find(':') + 2:answer.find('.')]
+    user_info.update({
+        'chat_id': event.user_id,
+        'question': random_question,
+        'answer': short_answer
+    })
+    redis_call.set(user_info['chat_id'], json.dumps(user_info))
+    logger.info(user_info['question'])
+    logger.info(user_info['answer'])
     return user_info['question']
-    
-    
-def handle_solution_attempt(event, vk_api):
-    user_info = get_user_info(event, vk_api)
+
+
+def handle_solution_attempt(event, vk_api, redis_call):
+    user_info = get_user_info(event.user_id, redis_call)
     if event.text == user_info['answer']:
         if 'score' in user_info:
             score = user_info['score']
         else:
             score = 0
-        score +=1
+        score += 1
         user_info['score'] = score
-        r.set(user_info['chat_id'], json.dumps(user_info))
-        return ''' Правильно! Поздравляю! 
-                                      Для следующего вопроса нажми 'Новый вопрос' 
+        redis_call.set(user_info['chat_id'], json.dumps(user_info))
+        return ''' Правильно! Поздравляю!
+                                      Для следующего вопроса нажми 'Новый вопрос'
                                   '''
     else:
-        return "Неправильно... Попробуешь ещё раз?"       
+        return "Неправильно... Попробуешь ещё раз?"
 
 
-def handle_hands_up(event, vk_api):
-    user_info = get_user_info(event, vk_api)
+def handle_hands_up(event, vk_api, redis_call):
+    user_info = get_user_info(event.user_id, redis_call)
     return user_info['answer']
-      
-         
+
 
 def send_score(event, vk_api):
-    user_info = get_user_info(event, vk_api)
+    user_info = get_user_info(event.use_id, redis_call)
     if user_info['score']:
         score = user_info['score']
     else:
         score = 0
     return f'Ваш счет:{score}'
-    
-    
+
+
 def make_keyboard():
     keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('Новый вопрос', color = VkKeyboardColor.PRIMARY)
-    keyboard.add_button('Сдаться', color = VkKeyboardColor.POSITIVE)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.POSITIVE)
     keyboard.add_line()
-    keyboard.add_button('Мой счёт', color = VkKeyboardColor.NEGATIVE)
+    keyboard.add_button('Мой счёт', color=VkKeyboardColor.NEGATIVE)
     return keyboard
-    
-     
-def send_msg(event, vk_api):
+
+
+def send_msg(event, vk_api, redis_call):
     keyboard = make_keyboard()
     if event.text == 'Новый вопрос':
-        msg = handle_new_question_request(event, vk_api)
+        msg = handle_new_question_request(event, vk_api, redis_call)
     elif event.text == 'Сдаться':
-        msg = handle_hands_up(event, vk_api)
+        msg = handle_hands_up(event, vk_api, redis_call)
     elif event.text == 'Мой счёт':
-        msg = send_score(event, vk_api)
-    else: msg = handle_solution_attempt(event, vk_api)
-    vk_api.messages.send(
-        user_id=event.user_id,
-        random_id=random.randint(1,1000),
-        keyboard=keyboard.get_keyboard(),
-        message=msg)
-    logger.info ('сообщение отправлено')
-
+        msg = send_score(event, vk_api, redis_call)
+    else:
+        msg = handle_solution_attempt(event, vk_api, redis_call)
+    vk_api.messages.send(user_id=event.user_id,
+                         random_id=random.randint(1, 1000),
+                         keyboard=keyboard.get_keyboard(),
+                         message=msg)
+    logger.info('сообщение отправлено')
 
 
 def start_bot():
+    load_dotenv()
+
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
 
     vk_session = vk.VkApi(token=os.getenv('VK_TOKEN'))
     vk_api = vk_session.get_api()
-       
-    r = redis.Redis(host=os.getenv('REDIS_ENDPOINT'),
-                    port=os.getenv('REDIS_PORT'),
-                    password=os.getenv('REDIS_PASSWORD'), db=0)
-                    
+
+    redis_call = redis.Redis(host=os.getenv('REDIS_ENDPOINT'),
+                             port=os.getenv('REDIS_PORT'),
+                             password=os.getenv('REDIS_PASSWORD'),
+                             db=0)
+
     longpoll = VkLongPoll(vk_session)
-                    
-    quiz_bases = get_quiz_bases('quiz-questions')
-    logger.info ('Бот запущен')
+
+    logger.info('Бот запущен')
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            send_msg(event, vk_api)
-     
+            send_msg(event, vk_api, redis_call)
+
 
 if __name__ == '__main__':
-       
-    load_dotenv()
-    
-    logging.basicConfig(
-                  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                  level=logging.INFO
-                  )
-    
-
-
     start_bot()
-
-    
-    
-    
-    
-    
-    
-    
-    
-
